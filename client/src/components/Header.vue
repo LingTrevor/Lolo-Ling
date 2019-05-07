3bc20ab1d84d76ad66f83b968b023bbc2dba84c8
Trevor <lingtrevor@gmail.com>
2019-05-07 11:47:23 +0300

    Added Header and scss variables

---
+++ client/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <nav class="nav">
+      <h1>Lolo Website</h1>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "../scss/_variables.scss";
+header {
+  height: auto;
+  padding: 10px;
+  background-color: $color-primary-dark;
+
+  .nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+      padding: 5px;
+      color: $color-primary-green;
+    }
+  }
+}
+</style>
