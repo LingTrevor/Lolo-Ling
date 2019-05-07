ae80a02706e45559da84d147e175982204738834
Trevor <lingtrevor@gmail.com>
2019-05-07 12:59:28 +0300

    Created Container component, edited router & imported Modal to Home view

---
+++ client/src/components/Container.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <slot>No slot content</slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../scss/_variables.scss";
+.container {
+  width: 100%;
+  padding: 20px;
+  background-color: $color-container-bg;
+  height: 100%;
+}
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <Container>
+    <FeedModal :isOpen="isOpen">
+      <div slot="close">
+        <Button btnTitle="Close" @click.native="isOpen = false"></Button>
+      </div>
+    </FeedModal>
+  </Container>
+</template>
+
+<script>
+// @ is an alias to /src
+import Container from "@/components/Container";
+import FeedModal from "@/components/FeedModal";
+import Button from "@/components/Button";
+export default {
+  name: "home",
+  data() {
+    return {
+      isOpen: true
+    };
+  },
+  components: {
+    Container,
+    FeedModal,
+    Button
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
