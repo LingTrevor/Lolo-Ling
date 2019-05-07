9489dcdc34c3ea61ba95e001329d6cfd35b23208
Trevor <lingtrevor@gmail.com>
2019-05-07 11:51:07 +0300

    Edited App.vue scss and added Header component to it

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
   </div>
 </template>
 
+<script>
+import Header from "@/components/Header";
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100vh;
+  }
 }
 </style>
