43f0189515a0845ea9b6a4e5173eb39f64b44e41
Trevor <lingtrevor@gmail.com>
2019-05-07 15:58:30 +0300

    Created Feed-container component

---
+++ client/src/components/FeedContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="feed-container">
+    <slot>No Slot content</slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/_variables.scss";
+.feed-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 80%;
+  margin: 20px auto;
+  padding: 10px;
+  box-shadow: 0 1px 15px 1px $color-primary-dark;
+}
+</style>
