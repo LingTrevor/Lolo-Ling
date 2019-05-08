f0df0d38de3d60448bc4719dc35f5d626fea7973
Trevor <lingtrevor@gmail.com>
2019-05-08 16:08:28 +0300

    App.js deployment setup + client build

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const routes = require("./server/src/routes/route");
 const { PORT } = require("./server/src/config/config");
 const app = express();
@@ -8,6 +8,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(routes);
+
+if (process.env.NODE_ENV === "production") {
+  //Static folder
+  app.use(express.static(__dirname + "/server/src/client/public"));
+
+  //Handle SPA
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + "/server/src/client/public/index.html");
+  });
+};
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
